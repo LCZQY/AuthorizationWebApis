df3ec9ecb51813efa8119d931d76de0a435e3534
郑强勇 <2536156085@QQ.com>
2018-12-07 13:13:26 +0800

    测似

--- ZQY_AuthorityManagement/Startup.cs
+++ ZQY_AuthorityManagement/Startup.cs
@@ -27,7 +27,7 @@ namespace ZQY_AuthorityManagement
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-
+        //测试
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
